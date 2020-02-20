efa5e1fc30f90f969c9414a42405721c4ff3d2ec
Sandra Tatarevićová <sandra.tatarevicova@ackee.cz>
2020-02-20 09:05:55 +0100

    Load NVM

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # Required variables:
 #   GCLOUD_SA_KEY: base64 encoded string with service account key (cat /pat/to/key | base64 -w0)
@@ -10,6 +10,9 @@
 echo "Initializing gcloud auth"
 echo $GCLOUD_SA_KEY | base64 -d > /tmp/key.json
 gcloud auth activate-service-account --key-file=/tmp/key.json
+
+# load NVM
+source /usr/local/nvm/nvm.sh
 
 # Run the original enrypoint
 exec /usr/local/bin/jenkins-slave "$@"
